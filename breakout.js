25d7df43fe9e77e1232917828c3007af7e564874
Jamie Miller <miller0328@gmail.com>
2019-10-20 22:45:06 -0400

    added ball and brick rows

--- breakout.js
+++ breakout.js
@@ -36,10 +36,46 @@ function preload() {
 
 function create() { 
     player = this.physics.add.sprite(
-        400, // x position
-        600, // y position
+        400, // x-position
+        600, // y-position
         'paddle', // key of image for the sprite
     );
+
+    ball = this.physics.add.sprite(
+        400, // x-position
+        565, // y-position
+        'ball' // key of image for the sprite
+    );
+
+    violetBricks = this.physics.add.group({
+        key: 'brick1',
+        repeat: 9, // tells how many more times to create a sprite 
+        setXY: {
+            x: 80,
+            y: 140,
+            stepX: 70 // length in pixels between repeated sprites on x-axis
+        }
+    });
+
+    yellowBricks = this.physics.add.group({
+        key: 'brick2',
+        repeat: 9,
+        setXY: {
+            x: 80,
+            y: 90,
+            stepX: 70
+        }
+    });
+
+    redBricks = this.physics.add.group({
+        key: 'brick3',
+        repeat: 9,
+        setXY: {
+            x: 80,
+            y: 40,
+            stepX: 70
+        }
+    });
 }
 
 function update() { }
